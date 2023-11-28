d84ce51c6016931f0554a58164575ace071dec7c
Aliye Idris <aliyeidiris@gmail.com>
2023-11-28 12:07:28 +0300

    Ide Setting

--- src/test/java/seleniummaster/AppiumBasics.java
+++ src/test/java/seleniummaster/AppiumBasics.java
@@ -1,21 +1,11 @@
 package seleniummaster;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
+
 
 /**
  * @author : user
